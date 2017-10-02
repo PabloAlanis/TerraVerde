177d56d6ae6f2d532928de501ca0c8069cd5c4c1
PabloAlanis <pabloalanis_1@hotmail.com>
2017-10-02 00:12:54 -0300

    comienzo vista inicio

---
+++ application/views/inicio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Terra Verde</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>recursos/bootstrap.min.css"/>
+  <script type="text/javascript" src="<?php echo base_url(); ?>recursos/jquery-3.2.1.js"/></script>
+  <link rel="stylesheet"  href="<?php echo base_url(); ?>/recursos/animate.css"/>
+  <script src="<?php echo base_url(); ?>recursos/bootstrap.min.js"/></script>
+</head>
+<body>
+Pantalla inicial
+
+
+
+
+
+
+
+</body>
+</html>
